fc6c4a68b82c857ace3d12ae05b1f14a5d2246b6
H Yu <etoast@hs-macbook-pro.myfiosgateway.com>
2023-02-15 12:48:29 -0500

    socket io implementation

---
+++ server/io.js
@@ -0,0 +1,39 @@
+const socketIo = require("socket.io");
+
+class SocketIo {
+  static instance;
+
+  static init(server) {
+    SocketIo.instance = socketIo(server, {
+      cors: {
+        origin: "*",
+      },
+    });
+  }
+
+  static start() {
+    if (!SocketIo.instance) {
+      throw Error ("socket.io instance not initialized");
+    }
+
+    SocketIo.instance.on("connection", (client) => {
+      console.log(client.id, "has connected");
+
+      client.on("joinPoll", (pollId) => {
+        console.log(client.id, "has joined", pollId);
+        client.join(pollId);
+      });
+
+      client.on("message", (data) => {
+        console.log(data);
+        SocketIo.instance.to(data.pollId).emit("message", data.message);
+      });
+
+      client.on("disconnect", () => {
+        console.log(client.id, "has disconnected");
+      });
+    });
+  }
+}
+
+module.exports = SocketIo;
