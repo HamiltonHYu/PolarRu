b90acd38e2f6bfaa506374018078b4b883bea55d
H Yu <etoast@hs-macbook-pro.myfiosgateway.com>
2023-02-15 11:27:51 -0500

    server logic addition

--- server/server.js
+++ server/server.js
@@ -64,3 +64,50 @@ app.post(
     }
   }
 );
+
+app.post(
+  "/logout",
+  sessionController.deleteSession,
+  cookieController.deleteCookie,
+  (req, res) => {
+    try {
+      return res.status(200).redirect("/");
+    } catch (err) {
+      res.status(200).json({tabs: "logout", message: "error in logout"});
+    }
+  }
+);
+
+const pollRouter = require("./routers/poll/js");
+app.use("/poll", pollRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).send("Not Found");
+})
+
+app.use((err, req, res, next) => {
+  console.log("Error from global error handler", err.log);
+  res.status(err.status || 500).send(err.message);
+});
+
+const runServer = async () => {
+  console.log("environment:", NODE_ENV);
+  await mongoose.connect(NODE_ENV === "test" ?
+  MONGO_TEST_URI : MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const server = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+  SocketIo.init(server);
+  SocketIo.start();
+
+  return async () => {
+    server.close();
+    return WSServer.socket.close();
+  };
+};
+
+module.export = runServer
