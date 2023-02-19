d7218e95d60f727800c3e20595ac191680793a68
H Yu <etoast@hs-macbook-pro.myfiosgateway.com>
2023-02-19 11:40:26 -0500

    poll models

---
+++ server/models/pollModels.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  pollsList: [{ type: Schema.Types.ObjectId, ref: "Poll" }],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const pollSchema = new Schema({
+  method: { type: String, required: true },
+  question: { type: String, required: true },
+  options: [String],
+  creatorId: {
+    // type: Schema.Types.ObjectId,
+    type: String,
+    // ref: "User",
+    required: true,
+  },
+  pollId: { type: String, required: true },
+  voteCount: Number,
+  responses: [
+    {
+      userId: String,
+      vote: Number,
+    },
+  ],
+  joined: [String],
+  winner: {
+    option: String,
+    count: Number,
+  },
+  active: Boolean,
+});
+
+const Poll = mongoose.model("Poll", pollSchema);
+
+module.exports = {
+  User,
+  Poll,
+};
